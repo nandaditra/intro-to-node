13a31296aa36507905d4e64253e36484bc4c1a74
Nanda Aditya Putra <88027838+nandaditra@users.noreply.github.com>
2022-01-05 16:54:49 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+const MySuperheroesName = superheroes.random();
+const MySupervillainsName = supervillains.random();
+
+console.log(MySuperheroesName);
+console.log(MySupervillainsName);
